058cb86b295e29d159c5780808eea9c845d1eb20
vanessapigwin <72374043+vanessapigwin@users.noreply.github.com>
2023-09-27 15:07:04 +0800

    initial commit, meow

---
+++ README.md
@@ -0,0 +1,8 @@
+# PLZ KITTY
+You don't need a reason to get a kitty picture
+
+
+### Why?
+- Actually a homework for TOP (fetch lesson)
+- Because cats
+- Because thecatapi.com is amazing!
---
+++ index.js
@@ -0,0 +1,31 @@
+(()=> {
+    const goKitty = document.querySelector('#plzkitty');
+    const litterBox = document.querySelector('#litterbox');
+    const defaultImgSrc = '.static/227.jpg';
+    const url = 'https://api.thecatapi.com/v1/images/search';
+    const copyArea = document.querySelector('#apiurl');
+    const copyButton = document.querySelector('#copybutton');
+
+    const findKitty = () => {
+        fetch(url)
+            .then((r) => r.json())
+            .then((r) => {
+                litterBox.src = r[0].urls
+                copyArea.value = r[0].url;
+            })
+            .catch((err) => {
+                litterBox.src = defaultImgSrc;
+                copyArea.value = 'not your kitty, iz Fr0m rep0';
+                console.log(err)
+            })
+    }
+
+    const clipboardCopy = () => {
+        copyArea.select();
+        copyArea.setSelectionRange(0, 99999);
+        navigator.clipboard.writeText(copyArea.value);
+    }
+
+    goKitty.addEventListener('click', findKitty);
+    copyButton.addEventListener('click', clipboardCopy)
+})();
